b6b72cd64b24844f8facefb22580ae67e836ed89
Siwaÿll <sanath.labs@gmail.com>
2014-09-13 16:03:43 +0200

    mep de la gestion de scénario

---
+++ Scenari.php
@@ -0,0 +1,220 @@
+<?php
+
+namespace Siwayll\Histoire;
+
+use \Exception;
+
+class Scenari
+{
+    private $order = [];
+    private $choices = [];
+    private $results = [];
+    private $current;
+
+    private $instructions = [
+        '_nextChoice' => 'setCurrentTo',
+        '_addChoice'  => 'addChoice',
+        '_addAtEnd'   => 'addChoices',
+    ];
+
+
+
+    public function __construct(array $data)
+    {
+        if (empty($data)) {
+            throw new Exception('Le scenario doit être un tableau non vide.', 400);
+        }
+
+        if (!isset($data['choices'])) {
+            throw new Exception('Le scenario doit être un tableau non vide.', 400);
+        }
+
+        foreach ($data['choices'] as $choiceData) {
+            $choice = new Choice($choiceData);
+            $this->choices[$choice->getName()] = $choice;
+        }
+
+        if (!isset($data['order'])) {
+            $this->order = array_keys($this->choices);
+        } else {
+            $this->order = $data['order'];
+        }
+
+        foreach ($this->order as $key => $value) {
+            $this->order[$key] = $value . uniqid('[%]');
+        }
+
+        reset($this->choices);
+        reset($this->order);
+
+        $this->current = current($this->order);
+    }
+
+    /**
+     * Change de choix en cours de réalisation
+     *
+     * @param string $name nom du choix à faire passer "en cours"
+     *
+     * @return self
+     * @throws Exception si aucun choix ne répond au nom demandé
+     */
+    public function setCurrentTo($name)
+    {
+        if (!isset($this->choices[$name])) {
+            throw new Exception('Aucun choix n\'a le nom _' . $name . '_', 400);
+        }
+        $name .= uniqid('[%]');
+        $position = array_search($this->current, $this->order) + 1;
+        $base = array_splice($this->order, 0, $position);
+        $this->order = array_merge($base, [$name], $this->order);
+
+        $this->restorePosition();
+
+        return $this;
+    }
+
+    /**
+     * Renvois les données du choix demandé
+     *
+     * @param string $name nom du choix
+     *
+     * @return Choice
+     * @throws Exception si aucun choix ne répond au nom demandé
+     */
+    public function getChoice($name)
+    {
+        if (!isset($this->choices[$name])) {
+            throw new Exception('Aucun choix n\'a le nom _' . $name . '_', 400);
+        }
+
+        return $this->choices[$name];
+    }
+
+    public function addChoice($name)
+    {
+        $name .= uniqid('[%]');
+        $this->order[] = $name;
+
+        return $this;
+    }
+
+    public function addChoices(array $names)
+    {
+        $names = array_map(function ($name) {
+            $name .= uniqid('[%]');
+            return $name;
+        }, $names);
+        $this->order = array_merge($this->order, $names);
+        $this->restorePosition();
+        return $this;
+    }
+
+    /**
+     * Restore la position du current après une reconstruction de l'ordre
+     *
+     * @return self
+     */
+    protected function restorePosition()
+    {
+        reset($this->order);
+        while (current($this->order) !== $this->current) {
+            next($this->order);
+        }
+
+        return $this;
+    }
+
+    /**
+     * Indique si il reste des choix à faire
+     *
+     * @return boolean
+     */
+    public function hasNextChoice()
+    {
+        if ($this->current === false) {
+            return false;
+        }
+
+        return true;
+    }
+
+    /**
+     * Renvois le choix en cours de réalisation
+     *
+     * @return Choice
+     */
+    public function getCurrent()
+    {
+        if ($this->current === false) {
+            return false;
+        }
+        $name = preg_replace('#(\[\%\][a-z0-9]+)$#', '', $this->current);
+        return $this->getChoice($name);
+    }
+
+    protected function updateScenari($commands)
+    {
+        foreach ($commands as $command => $option) {
+            $funcName = $this->instructions[$command];
+            $this->$funcName($option);
+        }
+    }
+
+    protected function update($command)
+    {
+        foreach ($command as $name => $update) {
+            if ($name == '_scenari') {
+                $this->updateScenari($update);
+                continue;
+            }
+
+            $this->updateChoice($name, $update);
+        }
+
+        return $this;
+    }
+
+    protected function updateChoice($name, $command)
+    {
+        $choice = $this->getChoice($name);
+        foreach ($command as $name => $parameter) {
+            $choice->update($name, $parameter);
+        }
+
+        return $this;
+    }
+
+
+    /**
+     * Enregistre le resultat du choix en cours
+     *
+     * @param type $name
+     * @return \Siwayll\Histoire\Scenari
+     */
+    public function setChoiceResult($name)
+    {
+        $choice = $this->getCurrent();
+        $result = $choice->getOption($name);
+
+        $result['_choiceName'] = $choice->getName();
+        $this->results[] = $result;
+
+        if (isset($result['onSelect'])) {
+            $this->update($result['onSelect']);
+        }
+
+        $next = next($this->order);
+
+        $this->current = $next;
+        if ($next === false) {
+            $this->current = false;
+        }
+        return $this;
+    }
+
+
+    public function getResults()
+    {
+        return $this->results;
+    }
+}
---
+++ tests/units/Scenari.php
@@ -0,0 +1,218 @@
+<?php
+/**
+ *
+ *
+ * @author  Siwaÿll <sana.th.labs@gmail.com>
+ * @license beerware http://wikipedia.org/wiki/Beerware
+ */
+
+namespace tests\unit\Siwayll\Histoire;
+
+use atoum;
+
+/**
+ *
+ *
+ * @author  Siwaÿll <sana.th.labs@gmail.com>
+ * @license beerware http://wikipedia.org/wiki/Beerware
+ */
+class Scenari extends atoum
+{
+    private function getChoiceOne()
+    {
+        $choice = [
+            'name' => 'yeux',
+            'options' => [
+                [
+                    'name' => 'y-1',
+                    'text' => 'bleu',
+                    'weight' => 50,
+                ],
+                [
+                    'name' => 'y-2',
+                    'text' => 'marron',
+                    'weight' => 129,
+                ],
+                [
+                    'name' => 'y-3',
+                    'text' => 'vert',
+                    'weight' => 20,
+                ],
+                [
+                    'name' => 'y-4',
+                    'text' => 'hétérochromie',
+                    'weight' => 1,
+                ],
+            ]
+        ];
+
+        return $choice;
+    }
+
+    private function getChoiceTwo()
+    {
+        $choice = [
+            'name' => 'cheveux',
+            'options' => [
+                [
+                    'name' => 'c-1',
+                    'text' => 'brun',
+                    'weight' => 50,
+                ],
+                [
+                    'name' => 'c-2',
+                    'text' => 'blond',
+                    'weight' => 129,
+                ],
+                [
+                    'name' => 'c-3',
+                    'text' => 'vert',
+                    'weight' => 20,
+                ],
+                [
+                    'name' => 'c-4',
+                    'text' => 'chauve',
+                    'weight' => 1,
+                ],
+            ]
+        ];
+
+        return $choice;
+    }
+
+    /**
+     * Renvois un scenario de choix très simple
+     *
+     * @return array
+     */
+    private function getScenariOne()
+    {
+        $scenari = [
+            'name'    => 'test',
+            'choices' => [$this->getChoiceOne(), $this->getChoiceTwo()],
+        ];
+
+        return $scenari;
+    }
+
+// -------------------------------------------------------------------------- //
+
+    /**
+     * Instantiation d'un scenario
+     *
+     * @return void
+     */
+    public function testConstruct()
+    {
+        $this
+            ->exception(function () {
+                $choice = new \Siwayll\Histoire\Scenari([]);
+            })
+                ->hasMessage('Le scenario doit être un tableau non vide.')
+                ->hasCode(400)
+            ->exception(function () {
+                $data = $this->getScenariOne();
+                unset($data['choices'][0]['name']);
+                $choice = new \Siwayll\Histoire\Scenari($data);
+            })
+                ->hasMessage('Utilisation d\'un choix sans nom impossible.')
+                ->hasCode(400)
+            ->exception(function () {
+                $data = $this->getScenariOne();
+                unset($data['choices'][0]['name']);
+                $choice = new \Siwayll\Histoire\Scenari($data);
+            })
+                ->hasMessage('Utilisation d\'un choix sans nom impossible.')
+                ->hasCode(400)
+            ->exception(function () {
+                $data = $this->getScenariOne();
+                unset($data['choices'][1]['options']);
+                $choice = new \Siwayll\Histoire\Scenari($data);
+            })
+                ->hasMessage('Le choix _cheveux_ doit avoir des options.')
+                ->hasCode(400)
+            ->exception(function () {
+                $data = $this->getScenariOne();
+                unset($data['choices'][0]['options'][0]['name']);
+                $choice = new \Siwayll\Histoire\Scenari($data);
+            })
+                ->hasMessage('Dans _yeux_ l\'option __0__ n\'a pas de nom')
+                ->hasCode(400)
+            ->exception(function () {
+                $data = $this->getScenariOne();
+                unset($data['choices'][0]['options'][1]['weight']);
+                $choice = new \Siwayll\Histoire\Scenari($data);
+            })
+                ->hasMessage('Dans _yeux_ __weight__ est manquant pour _y-2_')
+                ->hasCode(400)
+        ;
+    }
+
+    /**
+     * Contrôle du renvois du choix en cours de traitement
+     *
+     * @return void
+     */
+    public function testGetCurrent()
+    {
+        $this
+            ->if($scenari = new \Siwayll\Histoire\Scenari($this->getScenariOne()))
+            ->and($choice = new \Siwayll\Histoire\Choice($this->getChoiceOne()))
+            ->object($scenari->getCurrent())
+                ->isEqualTo($choice)
+            ->object($scenari->getCurrent())
+                ->isEqualTo($choice)
+        ;
+    }
+
+    public function testSetCurrentTo()
+    {
+        $this
+            ->if($scenari = new \Siwayll\Histoire\Scenari($this->getScenariOne()))
+            ->object($scenari->setCurrentTo('yeux'))
+                ->isIdenticalTo($scenari)
+            ->exception(function () use ($scenari) {
+                $scenari->setCurrentTo('dsfsdg');
+            })
+                ->hasMessage('Aucun choix n\'a le nom _dsfsdg_')
+                ->hasCode(400)
+        ;
+    }
+
+    /**
+     * Contrôle du getter
+     *
+     * @return void
+     */
+    public function testGetChoice()
+    {
+        $this
+            ->if($scenari = new \Siwayll\Histoire\Scenari($this->getScenariOne()))
+            ->and($choice = new \Siwayll\Histoire\Choice($this->getChoiceOne()))
+            ->object($scenari->getChoice('yeux'))
+                ->isEqualTo($choice)
+            ->exception(function () use ($scenari) {
+                $scenari->getChoice('dsfsdg');
+            })
+                ->hasMessage('Aucun choix n\'a le nom _dsfsdg_')
+                ->hasCode(400)
+        ;
+    }
+
+    public function testSetChoiceResult()
+    {
+        $this
+            ->if($scenari = new \Siwayll\Histoire\Scenari($this->getScenariOne()))
+            ->object($scenari->setChoiceResult('y-2'))
+                ->isIdenticalTo($scenari)
+            ->if($choice = new \Siwayll\Histoire\Choice($this->getChoiceTwo()))
+            ->object($scenari->getCurrent())
+                ->isEqualTo($choice)
+            ->exception(function () use ($scenari) {
+                $scenari->setChoiceResult('y-2');
+            })
+                ->hasMessage('Dans _cheveux_ l\'option __y-2__ n\'existe pas')
+                ->hasCode(400)
+        ;
+    }
+}

24db882f6d3b77ef26250480ae20606585644a76
Siwaÿll <sanath.labs@gmail.com>
2015-07-11 10:24:18 +0200

    formalisation de Beauty

--- Modificator/Beauty.php
+++ Modificator/Beauty.php
@@ -3,6 +3,8 @@
 namespace Siwayll\Histoire\Modificator;
 
 use \Siwayll\Histoire\Register;
+use \Siwayll\Histoire\Error\Level;
+use \Exception;
 
 /**
  * Ajout de la notion d'experience
@@ -12,6 +14,25 @@ use \Siwayll\Histoire\Register;
  */
 class Beauty extends Base
 {
+    static protected $level = [
+        'Ignoble' => null,
+        '{Dégoûtant@HOMME|Dégoûtante@FEMME}' => -10000,
+        '{Affreux@HOMME|Affreuse@FEMME}' => -5000,
+        '{Laid@HOMME|Laide@FEMME}' => -3000,
+        'Pas très {beaux@HOMME|belle@FEMME}' => -1000,
+        'Passable' => 0,
+        '{Moyen@HOMME|Moyenne@FEMME}' => 1000,
+        '{Mignon@HOMME|Mignonne@FEMME}' => 3000,
+        '{Beau@HOMME|belle@FEMME}' => 5000,
+        'Très {Beau@HOMME|belle@FEMME}' => 10000,
+        'Très grand beauté' => 20000,
+        'Beauté incomparable' => 40000,
+        'Beauté divine' => 80000,
+    ];
+
+    protected $value = 0;
+
+    protected $history = [];
 
     /**
      * Renvoie le nom du modificateur
@@ -36,6 +57,14 @@ class Beauty extends Base
         return $instructions;
     }
 
+    public function getDatas()
+    {
+        return [
+            'value' => $this->value,
+            'history' => $this->history,
+        ];
+    }
+
     /**
      * Non utilisé
      *
@@ -50,6 +79,53 @@ class Beauty extends Base
     }
 
     /**
+     * Récupère les informations de la ligne d'option
+     *
+     * @Param string $option Ligne d'option
+     *
+     * @return int Valeur de variation
+     */
+    protected function parseOption($option)
+    {
+        $pattern = '/(?<word>[a-z:]+) ((?<sign>\+|-|~)(?<var>[0-9]+))/';
+        if (!preg_match($pattern, $option, $match)) {
+            throw new Exception(
+                '__beauty__ _' . $option . '_ mal formaté',
+                Level::NOTICE
+            );
+        }
+
+        // On applique la valeur négative si nécessaire
+        if ($match['sign'] == '-') {
+            $match['var'] = '-' . $match['var'];
+        }
+
+        return [
+            'value' => (int) $match['var'],
+            'sign' => $match['sign'],
+        ];
+    }
+
+    /**
+     * Lissage de la beauté
+     *
+     * @param int $div Valeur de division
+     *
+     * @return int Nouveau score de beauté
+     */
+    protected function smooth($div)
+    {
+        if ($this->value > 1000) {
+            $value = (($this->value - 1000) / $div) + 1000;
+        } else {
+            $value = (($this->value + 1000) / $div) - 1000;
+        }
+
+        return $value;
+
+    }
+
+    /**
      * Evolution du score de beautée
      *
      * @param int $option Modificateur du score de beauté
@@ -58,22 +134,36 @@ class Beauty extends Base
      */
     public function add($option)
     {
-        $value = (int) $option;
-        $engine = Register::load($this->engineKey);
-        $npc = $engine->getResult()->getStorage();
+        $this->history[] = $option;
 
-        if (!isset($npc->description->charme->history)) {
-            $npc->set([], 'description', 'charme', 'history');
+        $data = $this->parseOption($option);
+        switch ($data['sign']) {
+            case '~':
+                $this->value = $this->smooth($data['value']);
+                break;
+
+            default:
+                $this->value += $data['value'];
         }
-
-        $npc->description->charme->history[] = 'var ' . $option;
-
-        if (isset($npc->description->charme->score)) {
-            $value += $npc->description->charme->score;
-        }
-
-        $npc->set($value, 'description', 'charme', 'score');
 
         return null;
+    }
+
+    /**
+     * Renvoi la chaîne associé au score de beauté.
+     * La chaîne est compatible avec le mod Gender
+     *
+     * @return string
+     */
+    public function getLitteral()
+    {
+        $str = '';
+        foreach (self::$level as $text => $value) {
+            if ((int) $this->value >= $value) {
+                $str = $text;
+            }
+        }
+
+        return $str;
     }
 }

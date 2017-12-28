083018ff07effdd8e65b7ca546eedc78427474e0
Siwaÿll <siwayll@doublepsi.fr>
2017-12-28 17:01:50 +0100

    add doc

--- tests/units/Provider/SimpleChoiceTrait.php
+++ tests/units/Provider/SimpleChoiceTrait.php
@@ -6,8 +6,15 @@ use Gen3se\Engine\Choice\Choice;
 use Gen3se\Engine\Option\Collection;
 use Gen3se\Engine\Option\Option;
 
+/**
+ * Trait SimpleChoiceTrait
+ * @package Gen3se\Engine\Tests\Units\Provider
+ */
 trait SimpleChoiceTrait
 {
+    /**
+     * @return Choice
+     */
     protected function getEyeColorChoice()
     {
         $optCollection = new Collection();
